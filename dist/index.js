f6c805474ce8c009c40ae44f9d3be0d2891bff11
Russell Haering <russell@edgebit.io>
2023-12-07 08:09:05 -0800

    Compile release

--- dist/index.js
+++ dist/index.js
@@ -304,7 +304,7 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.getCLI = exports.uploadSBOM = void 0;
 const exec = __importStar(__nccwpck_require__(1514));
 const tc = __importStar(__nccwpck_require__(7784));
-const ebctlVersion = 'v0.6.1';
+const ebctlVersion = 'v0.7.0';
 async function uploadSBOM(params) {
     const ebctl = await getCLI();
     const args = ['upload-sbom'];
